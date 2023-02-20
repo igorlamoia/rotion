4029894d4aa15234598c1331e3b7b62c85afaf89
igor lamoia <igor@hbifinanceira.com.br>
2023-02-20 18:31:31 -0300

    chore: base do electron com eslint e google fonts configurados

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div className="container">
+      <h1 className="text-violet-400 font-sans font-bold text-md">Muito bom</h1>
+    </div>
+  )
+}
